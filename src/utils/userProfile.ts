1532fe0d9d4ccda9e2f6a5abd7b246ec4ac258b8
Levkov Gleb <levkov.gleb.dev@gmail.com>
2025-04-02 14:05:50 +0300

    feat: refactor profile updater

--- src/utils/userProfile.ts
+++ src/utils/userProfile.ts
@@ -1,41 +1,60 @@
-import { ai } from "@src/ai/client.ts";
 import type { BotContext, KvUser } from "@src/context.ts";
+import type { User } from "@grammy/types";
+import { ai } from "@src/ai/client.ts";
 import { db } from "@src/db.ts";
 
-const getUserKey = (id: string | number) => {
-  return ["users", String(id)];
-};
-
 export const updateProfile = async (
-  ctx: BotContext
+  ctx: BotContext,
 ): Promise<KvUser | undefined> => {
-  let user = undefined;
-  if (ctx.msg?.from?.id) {
-    const userKey = getUserKey(ctx.msg.from.id);
-    const userWrapper = await db.get<KvUser>(userKey);
-    if (!userWrapper.value) {
-      user = { history: [ctx.msg.text!], profile: undefined };
-      await db.set(userKey, user);
+  if (!ctx.msg?.from?.id) return;
+
+  const userFromKv = await db.get<KvUser>(["user", ctx.msg.from.id]);
+
+  let user = userFromKv.value ?? undefined;
+
+  if (!user) {
+    user = await createNewUser(ctx.msg.from);
+  }
+
+  if (ctx.msg.text) {
+    user.history.push(ctx.msg.text);
+  }
+
+  if (user.history.length > 30) {
+    const newProfile = await createProfile(user.history, user.profile);
+    if (newProfile) {
+      user.history = [];
+      user.profile = newProfile;
     } else {
-      if (userWrapper.value.history.length > 30) {
-        user = {
-          history: [ctx.msg.text],
-          profile: await createProfile(
-            userWrapper.value.history,
-            userWrapper.value.profile
-          ),
-        } as KvUser;
-        await db.set(userKey, user);
-      } else {
-        user = {
-          history: [...userWrapper.value.history, ctx.msg.text],
-          profile: userWrapper.value.profile,
-        } as KvUser;
-        await db.set(userKey, user);
-      }
+      console.warn(`Failed to update User: @${ctx.msg.from.username}`);
+      user.history = user.history.slice(0, Math.ceil(user.history.length / 2));
     }
   }
+
+  await db.set(["user", ctx.msg.from.id], user);
+
   return user;
+};
+
+const createNewUser = async (user: User): Promise<KvUser> => {
+  const profile = `
+  Стартовая характеристика пользователя:
+  Имя: ${user.first_name} ${user.last_name}
+  Никнейм: ${user.username}
+  `;
+
+  const newUser = { history: [], profile };
+
+  const result = await db.set(["user", user.id], newUser);
+
+  if (result.ok) {
+    console.log(
+      `New User created: @${user.username} - ${user.first_name} ${user.last_name}`,
+    );
+    return newUser;
+  } else {
+    throw Error("Error to create User.");
+  }
 };
 
 const createProfile = async (history: string[], profile?: string) => {
