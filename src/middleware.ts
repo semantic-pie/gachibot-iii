3285b2fc980eabb5fefec6e522c3ea5402eb151a
Levkov Gleb <90149444+glebchanskiy@users.noreply.github.com>
2025-04-03 09:46:40 +0300

    feat: token-based shouldIAnswer | skip old messages (#11)

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,16 @@ import type { BotContext } from "@src/context.ts";
 import { shouldIAnswer } from "@src/utils/shouldIAnswer.ts";
 import { isAdmin } from "@src/utils/userAdmin.ts";
 import { updateProfile } from "@src/utils/userProfile.ts";
+import { isOldMessage } from "@src/utils/isOldMessage.ts";
 
 export const middleware: Middleware<BotContext> = async (ctx, next) => {
+  const isExpired = isOldMessage(ctx)
+
   ctx.config = {
     user: await updateProfile(ctx),
     isAdmin: isAdmin(ctx.from?.id),
-    shouldBreakIn: await shouldIAnswer(ctx.msg?.text),
+    isOldMessage: isExpired,
+    shouldBreakIn: isExpired ? 0 : shouldIAnswer(ctx.msg?.text),
   };
 
   console.log("message recived: ", ctx.msg?.text)
